d7795e9d02010810162dcf507b4c6686bf41d302
Omar Montiel <OmarMontiel@Omars-MacBook-Pro-33.local>
2019-02-26 17:48:46 -0600

    Add Orientation_Sensor file and add BNO055 setup

--- Soccer_Lightweight/Soccer_Lightweight.ino
+++ Soccer_Lightweight/Soccer_Lightweight.ino
@@ -4,44 +4,52 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-/*BNO055*/
+/* BNO055 Variables*/
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
 int angulo = 0;
 unsigned long long tiempo = 0;
 
-/*TSOP  */
+/* TSOP Variables */
 /*const int IRSensor = ;
 int IRValor = 0;*/
 
-/*Motor 1*/
-const int motor1A = 6;
-const int motor1B = 7;
-
-/*Motor 2*/
-const int motor2A = 8;
-const int motor2B = 9;
-
-/*Motor 3*/
-const int motor3A = 10;
-const int motor3B = 11;
-
-/*/////////////////////////////Fotoresistencias//////////////////////*/
+/* Photoresistors Variables */
 int pinLDR = 0;
 int valorLDR = 0;  
 //int LD = 8;
 
-/*////////////////////PROGRAMACIÓN DEL SEEKER////////////////////////*/
+/* Motors Variables */
+const int motor1A = 6;
+const int motor1B = 7;
+
+const int motor2A = 8;
+const int motor2B = 9;
+
+const int motor3A = 10;
+const int motor3B = 11;
+
+/* Seeker Program */
 int DirectionAngle(byte Direction)
 {
   return Direction * 30 - 150;
 }
 
-/*Adafruit_BNO055 bno = Adafruit_BNO055(55);*/
-
 void setup()
 {
   Serial.begin(9600);
+
+  /* BNO055 Setup */
+  if(!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ...");
+  }
+  delay(1);
+  bno.setExtCrystalUse(true);
   
-  InfraredSeeker::Initialize();
+  /* Motors Setup */
   pinMode(motor1A, OUTPUT);
   pinMode(motor1B, OUTPUT);
   pinMode(motor2A, OUTPUT);
@@ -49,11 +57,13 @@ void setup()
   pinMode(motor3A, OUTPUT);
   pinMode(motor3B, OUTPUT);
 
+  /* Photoresistors Setup */
   pinMode(LD, OUTPUT);
   digitalWrite(LD, LOW);
   /*pinMode(IRSensor, INPUT);*/
 
-  /*bno.setExtCrystalUse(true);*/
+  /* Seeker Setup */
+  InfraredSeeker::Initialize();
 }
 
 void loop()
