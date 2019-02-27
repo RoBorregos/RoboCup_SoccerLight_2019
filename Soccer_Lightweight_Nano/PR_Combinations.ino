05cf0c50defbe0b3c480c1c37e22f53b5e4c6ebc
Omar Montiel <OmarMontiel@Omars-MacBook-Pro-33.local>
2019-02-27 17:56:22 -0600

    Fix PR_Combinations syntaxis

--- Soccer_Lightweight_Nano/PR_Combinations.ino
+++ Soccer_Lightweight_Nano/PR_Combinations.ino
@@ -1,116 +1,89 @@
-void Com_to_Mega (bool a, bool b, bool c, bool d, bool e) {
+void Com_to_Mega(bool a, bool b, bool c, bool d, bool e)
+{
   digitalWrite(C1, a);
   digitalWrite(C2, b);
   digitalWrite(C3, c);
   digitalWrite(C4, d);
-  digitalWrite(C5, e);
-  
-  }
-  
- void PR_Combinations() {
+  digitalWrite(C5, e);  
+}
+
+void PR_Combinations()
+{
   linea();
- /*/////////////////////////////////////////////First line of Photo Resistors//////////////////////////////////////////////
- 
- FL = First Line of photoresistors
- SL= Second Line of photoresistors
- TL = Third Line of photoresistors
- */
-  
-  if ((PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_First Plate 
+
+  /* First Level of Photoresistors */
+  if((PRs[0] > 400 || PRs[1] > 400) && !(PRs[2] > 400 || PRs[3] > 400) && !(PRs[4] > 400 || PRs[5] > 400)) //FL: First Plate 
+  {
     Com_to_Mega(0, 1, 0, 0, 0);
-    
-    }
-    
-  else if (!(PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_Second Plate 
+  }
+  else if(!(PRs[0] > 400 || PRs[1] > 400) && (PRs[2] > 400 || PRs[3] > 400) && !(PRs[4] > 400 || PRs[5] > 400)) //FL: Second Plate
+  {
     Com_to_Mega(0, 1, 0, 0, 1);
-    
-    }
-    
-  else if (!(PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_Third Plate 
+  }
+  else if(!(PRs[0] > 400 || PRs[1] > 400) && !(PRs[2] > 400 || PRs[3] > 400) && (PRs[4] > 400 || PRs[5] > 400)) //FL: Third Plate
+  {
     Com_to_Mega(0, 1, 0, 1, 0);
-    
-    }
-    
-  else if ((PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_First and Second Plates
+  }
+  else if((PRs[0] > 400 || PRs[1] > 400) && (PRs[2] > 400 || PRs[3] > 400) && !(PRs[4] > 400 || PRs[5] > 400)) //FL: First and Second Plates
+  {
     Com_to_Mega(0, 1, 0, 1, 1);
-    
-    }
-
-  else if (!(PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_Second and Third Plates
+  }
+  else if(!(PRs[0] > 400 || PRs[1] > 400) && (PRs[2] > 400 || PRs[3] > 400) && (PRs[4] > 400 || PRs[5] > 400)) //FL: Second and Third Plates
+  {
     Com_to_Mega(0, 1, 1, 0, 0);
-    
-    }
-    
-  else if ((PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_First and Third Plates
+  }
+  else if((PRs[0] > 400 || PRs[1] > 400) && !(PRs[2] > 400 || PRs[3] > 400) && (PRs[4] > 400 || PRs[5] > 400)) //FL: First and Third Plates
+  {
     Com_to_Mega(0, 1, 1, 0, 1);
-    
-    }
-
-
-  /*/////////////////////////////////////////////Second line of Photo Resistors//////////////////////////////////////////////*/
-
-if ((PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_First Plate 
+  }
+  /* Second Level of Photoresistors */
+  else if((PRs[6] > 400 || PRs[7] > 400) && !(PRs[8] > 400 || PRs[9] > 400) && !(PRs[10] > 400 || PRs[11] > 400)) //SL: First Plate 
+  {
     Com_to_Mega(1, 0, 0, 0, 0);
-    
-    }
-    
-  else if (!(PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_Second Plate 
+  }
+  else if(!(PRs[6] > 400 || PRs[7] > 400) && (PRs[8] > 400 || PRs[9] > 400) && !(PRs[10] > 400 || PRs[11] > 400)) //SL: Second Plate
+  {
     Com_to_Mega(1, 0, 0, 0, 1);
-    
-    }
-    
-  else if (!(PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_Third Plate 
+  }
+  else if(!(PRs[6] > 400 || PRs[7] > 400) && !(PRs[8] > 400 || PRs[9] > 400) && (PRs[10] > 400 || PRs[11] > 400)) //SL: Third Plate 
+  {
     Com_to_Mega(1, 0, 0, 1, 0);
-    
-    }
-    
-  else if ((PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_First and Second Plates
+  }
+  else if((PRs[6] > 400 || PRs[7] > 400) && (PRs[8] > 400 || PRs[9] > 400) && !(PRs[10] > 400 || PRs[11] > 400)) //SL: First and Second Plates
+  {
     Com_to_Mega(1, 0, 0, 1, 1);
-    
-    }
-
-  else if (!(PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_Second and Third Plates
+  }
+  else if(!(PRs[6] > 400 || PRs[7] > 400) && (PRs[8] > 400 || PRs[9] > 400) && (PRs[10] > 400 || PRs[11] > 400)) //SL: Second and Third Plates
+  {
     Com_to_Mega(1, 0, 1, 0, 0);
-    
-    }
-    
-  else if ((PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_First and Third Plates
+  }
+  else if((PRs[6] > 400 || PRs[7] > 400) && !(PRs[8] > 400 || PRs[9] > 400) && (PRs[10] > 400 || PRs[11] > 400)) //SL: First and Third Plates
+  {
     Com_to_Mega(1, 0, 1, 0, 1);
-    
-    }
-
-  /*/////////////////////////////////////////////Third line of Photo Resistors//////////////////////////////////////////////*/
-
-  else if ((PRs[12] > 400) && !(PRs[13] > 400) && !(PRs[14] > 400)) { // TL_FirstvPlate
+  }
+  /* Third Level of Photoresistors */
+  else if((PRs[12] > 400) && !(PRs[13] > 400) && !(PRs[14] > 400)) //TL: First Plate
+  {
     Com_to_Mega(1, 1, 0, 0, 0);
-     
-    }
-
-  else if (!(PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) { // TL_Second Plate
+  }
+  else if(!(PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) //TL: Second Plate
+  {
     Com_to_Mega(1, 1, 0, 0, 1);
-    
-    }
-    
-  else if (!(PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) { // TL_ Third Plate
+  }
+  else if(!(PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) //TL: Third Plate
+  {
     Com_to_Mega(1, 1, 0, 1, 0);
-    
-    }
-
-  else if ((PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) { // TL_First and Second Plates
+  }
+  else if((PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) //TL: First and Second Plates
+  {
     Com_to_Mega(1, 1, 0, 1, 1);
-    
-    }
-
-  else if (!(PRs[12] > 400) && (PRs[13] > 400) && (PRs[14] > 400)) { // TL_Second and Third Plates
+  }
+  else if(!(PRs[12] > 400) && (PRs[13] > 400) && (PRs[14] > 400)) //TL: Second and Third Plates
+  {
     Com_to_Mega(1, 1, 1, 0, 0);
-    
-    }
-
-  else if ((PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) { // TL_First and Third Plates
-    Com_to_Mega(1, 1, 1, 0, 1); 
-    
-    }
-  
-    
-
+  }
+  else if((PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) //TL: First and Third Plates
+  {
+    Com_to_Mega(1, 1, 1, 0, 1);
+  }
 }
