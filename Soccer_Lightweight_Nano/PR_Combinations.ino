b4435021ef5eb725283b9804225a500f16a438f7
Ricardo Abraham Chapa Romero <chaparomero@Ricardos-MacBook-Pro-85.local>
2019-02-26 21:57:54 -0600

    Modify the PR_Combination program

--- Soccer_Lightweight_Nano/PR_Combinations.ino
+++ Soccer_Lightweight_Nano/PR_Combinations.ino
@@ -1,4 +1,10 @@
-void Communication (bool ) {
+void Com_to_Mega(bool a, bool b, bool c, bool d, bool e) {
+
+  digitalWrite(x, a);
+  digitalWrite(x, b);
+  digitalWrite(x, c);
+  digitalWrite(x, d);
+  digitalWrite(x, e);
   
   }
 
@@ -7,29 +13,40 @@ void PR_Combinations() {
   
 linea();
 
- /*/////////////////////////////////////////////First line of Photo Resistors//////////////////////////////////////////////*/
+ /*/////////////////////////////////////////////First line of Photo Resistors//////////////////////////////////////////////
+ 
+ FL = First Line of photoresistors
+ SL= Second Line of photoresistors
+ TL = Third Line of photoresistors
+ */
   
   if ((PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_First Plate 
+    Com_to_Mega();
     
     }
     
   else if (!(PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_Second Plate 
+    Com_to_Mega();
     
     }
     
   else if (!(PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_Third Plate 
+    Com_to_Mega();
     
     }
     
   else if ((PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && !(PRs[4] > 400 ||PRs[5] > 400)) { // FL_First and Second Plates
+    Com_to_Mega();
     
     }
 
   else if (!(PRs[0] > 400 ||PRs[1] > 400) && (PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_Second and Third Plates
+    Com_to_Mega();
     
     }
     
   else if ((PRs[0] > 400 ||PRs[1] > 400) && !(PRs[2] > 400 ||PRs[3] > 400) && (PRs[4] > 400 ||PRs[5] > 400)) { // FL_First and Third Plates
+    Com_to_Mega();
     
     }
 
@@ -37,52 +54,64 @@ linea();
   /*/////////////////////////////////////////////Second line of Photo Resistors//////////////////////////////////////////////*/
 
 if ((PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_First Plate 
+    Com_to_Mega();
     
     }
     
   else if (!(PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_Second Plate 
+    Com_to_Mega();
     
     }
     
   else if (!(PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_Third Plate 
+    Com_to_Mega();
     
     }
     
   else if ((PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && !(PRs[10] > 400 ||PRs[11] > 400)) { // SL_First and Second Plates
+    Com_to_Mega();
     
     }
 
   else if (!(PRs[6] > 400 ||PRs[7] > 400) && (PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_Second and Third Plates
+    Com_to_Mega();
     
     }
     
   else if ((PRs[6] > 400 ||PRs[7] > 400) && !(PRs[8] > 400 ||PRs[9] > 400) && (PRs[10] > 400 ||PRs[11] > 400)) { // SL_First and Third Plates
+    Com_to_Mega();
     
     }
 
   /*/////////////////////////////////////////////Third line of Photo Resistors//////////////////////////////////////////////*/
 
   else if ((PRs[12] > 400) && !(PRs[13] > 400) && !(PRs[14] > 400)) { // TL_FirstvPlate
-    
+    Com_to_Mega();
+     
     }
 
   else if (!(PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) { // TL_Second Plate
+    Com_to_Mega();
     
     }
     
   else if (!(PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) { // TL_ Third Plate
+    Com_to_Mega();
     
     }
 
   else if ((PRs[12] > 400) && (PRs[13] > 400) && !(PRs[14] > 400)) { // TL_First and Second Plates
+    Com_to_Mega();
     
     }
 
   else if (!(PRs[12] > 400) && (PRs[13] > 400) && (PRs[14] > 400)) { // TL_Second and Third Plates
+    Com_to_Mega();
     
     }
 
   else if ((PRs[12] > 400) && !(PRs[13] > 400) && (PRs[14] > 400)) { // TL_First and Third Plates
+    Com_to_Mega(); 
     
     }
   
