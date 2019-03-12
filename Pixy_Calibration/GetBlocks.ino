19582a55638a5a5694d8283438d904c26ca7890f
Omar Montiel <OmarMontiel@Omars-MacBook-Pro-59.local>
2019-03-11 20:19:56 -0600

    Add center to Pixy calibration

--- Pixy_Calibration/GetBlocks.ino
+++ Pixy_Calibration/GetBlocks.ino
@@ -1,22 +1,28 @@
-void block() {
-   int i; 
+void block()
+{
+  int i; 
   pixy.ccc.getBlocks(); // grab blocks!
-  
-  
-  if (pixy.ccc.numBlocks) // If there are detect blocks, print them!
+
+  if(pixy.ccc.numBlocks) // If there are detect blocks, print them!
   {
-    Serial.print("Detected ");
-    for (i=0; i<pixy.ccc.numBlocks; i++)
+    Serial.print("Detected block on: ");
+    for (i=0; i < pixy.ccc.numBlocks; i++)
     
       
-  if (pixy.ccc.blocks[i].m_x < 150) {
-    Serial.println("On range");
-    delay(100);
-     }
-
-    else{
-    Serial.println("Not on range");
-      }
-       
-   } 
- }
+    if(pixy.ccc.blocks[i].m_x < 125)
+    {
+      Serial.println("Right");
+      delay(100);
+    }
+    else if(pixy.ccc.blocks[i].m_x < 175)
+    {
+      Serial.println("Center");
+      delay(100);
+    }
+    else
+    {
+      Serial.println("Left");
+      delay(100);
+    }
+  }
+}
